1924f1ee236227e0526f4f737e18e311ef811043
rehpinheirooo <renato.pinheiroo2025@gmail.com>
2025-12-24 02:40:38 -0300

    fix: ajustar import do app para execução correta no CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,10 @@
-o commit changes ? from src.app import app
+import sys
+sys.path.append("src")
+
+from app import app
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert "TechFlow Task Manager" in response.data.decode()
-    
